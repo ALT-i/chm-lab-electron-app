5131cb3586ce784ea4721f988c0b99de87c48fe6
Pamilerin Idowu <pamilerinid@gmail.com>
2024-05-01 13:51:00 +0100

    fix: update landing page

--- src/renderer/components/sections/SectionMainHeader.tsx
+++ src/renderer/components/sections/SectionMainHeader.tsx
@@ -3,26 +3,52 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useWindowStore } from 'renderer/store'
 
+import packageJson from '../../../../package.json'
+import { Typography } from '@material-tailwind/react'
+
 function SectionMainHeader() {
   const navigate = useNavigate()
   const store = useWindowStore().about
 
   return (
     <div className="main-header">
-      <div className="flex items-center justify-center">
-        <img className="img-circle" src="./noun_logo.png" alt="" />
-        <img className="img-circle" src="./acetel_logo.png" alt="" />
+      <div className="flex items-center justify-center mb-20">
+        <img
+          className="img-circle !w-80 !h-80 !p-0"
+          src="./noun_logo.png"
+          alt=""
+        />
+        <img
+          className="img-circle !w-72 !h-72"
+          src="./acetel_logo.png"
+          alt=""
+        />
       </div>
       <div className="">
-        <h2 className="side-banner-text mt-5 mb-8">
+        <Typography
+          color="lime"
+          className="font-normal flex w-full justify-center items-center text-2xl m-8 p-8"
+          placeholder={''}
+          variant="h6"
+        >
+          {/* <h2 className="side-banner-text mt-5 mb-8"> */}
           National Open University of Nigeria (NOUN)
           <br />
           Africa Centre of Excellence on Technology Enhanced Learning (ACETEL)
-        </h2>
-        <h3 className="subtext mt-12">
+          {/* </h2> */}
+        </Typography>
+        <div className="subtext">
           {/* <p onClick={() => navigate('anotherScreen')} className='subtext'> */}
-          <p className="subtext">CHM Virtual Lab</p>
-        </h3>
+          <Typography
+            color="white"
+            className="font-normal flex w-full justify-center items-center text-2xl m-3 p-3"
+            placeholder={''}
+            variant="h6"
+          >
+            Virtual Chemistry Lab
+          </Typography>
+          <p className="italic text-lime-200">v{packageJson.version}</p>
+        </div>
       </div>
       {/* <button onClick = {() => {
                 electron.notificationApi.sendNotification('My custom notification')
